72b4ab6573517c500326e7a5ba1696690952c2af
mgryszko <marcin.gryszko@lastminute.com>
2020-12-05 15:23:49 +0100

    Use Position without converting from tuple

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -4,41 +4,39 @@ class Test extends AnyFunSuite {
   import MarsRover._
 
   test("move forward") {
-    assert(moveForward(MarsRover((0, 0), North)) == MarsRover((0, 1), North))
-    assert(moveForward(MarsRover((0, 0), East)) == MarsRover((1, 0), East))
-    assert(moveForward(MarsRover((0, 0), South)) == MarsRover((0, -1), South))
-    assert(moveForward(MarsRover((0, 0), West)) == MarsRover((-1, 0), West))
+    assert(moveForward(MarsRover(Position(0, 0), North)) == MarsRover(Position(0, 1), North))
+    assert(moveForward(MarsRover(Position(0, 0), East)) == MarsRover(Position(1, 0), East))
+    assert(moveForward(MarsRover(Position(0, 0), South)) == MarsRover(Position(0, -1), South))
+    assert(moveForward(MarsRover(Position(0, 0), West)) == MarsRover(Position(-1, 0), West))
   }
 
   test("move backward") {
-    assert(moveBackward(MarsRover((0, 0), North)) == MarsRover((0, -1), North))
-    assert(moveBackward(MarsRover((0, 0), East)) == MarsRover((-1, 0), East))
-    assert(moveBackward(MarsRover((0, 0), South)) == MarsRover((0, 1), South))
-    assert(moveBackward(MarsRover((0, 0), West)) == MarsRover((1, 0), West))
+    assert(moveBackward(MarsRover(Position(0, 0), North)) == MarsRover(Position(0, -1), North))
+    assert(moveBackward(MarsRover(Position(0, 0), East)) == MarsRover(Position(-1, 0), East))
+    assert(moveBackward(MarsRover(Position(0, 0), South)) == MarsRover(Position(0, 1), South))
+    assert(moveBackward(MarsRover(Position(0, 0), West)) == MarsRover(Position(1, 0), West))
   }
 }
 
 object MarsRover {
-  implicit def tupleToPosition(pos: (Int, Int)): Position = Position(pos._1, pos._2)
-
   def moveForward(rover: MarsRover): MarsRover = rover.direction match {
-    case North => rover.copy(position = (rover.position._1, rover.position._2 + 1))
-    case East =>rover.copy(position = (rover.position._1 + 1, rover.position._2))
-    case South =>rover.copy(position = (rover.position._1, rover.position._2 - 1))
-    case West =>rover.copy(position = (rover.position._1 - 1, rover.position._2))
+    case North => rover.copy(position = Position(rover.position.x, rover.position.y + 1))
+    case East => rover.copy(position = Position(rover.position.x + 1, rover.position.y))
+    case South => rover.copy(position = Position(rover.position.x, rover.position.y - 1))
+    case West => rover.copy(position = Position(rover.position.x - 1, rover.position.y))
   }
 
   def moveBackward(rover: MarsRover): MarsRover = rover.direction match {
-    case North => rover.copy(position = (rover.position._1, rover.position._2 - 1))
-    case East =>rover.copy(position = (rover.position._1 - 1, rover.position._2))
-    case South =>rover.copy(position = (rover.position._1, rover.position._2 + 1))
-    case West =>rover.copy(position = (rover.position._1 + 1, rover.position._2))
+    case North => rover.copy(position = Position(rover.position.x, rover.position.y - 1))
+    case East => rover.copy(position = Position(rover.position.x - 1, rover.position.y))
+    case South => rover.copy(position = Position(rover.position.x, rover.position.y + 1))
+    case West => rover.copy(position = Position(rover.position.x + 1, rover.position.y))
   }
 }
 
 case class MarsRover(position: Position, direction: Direction)
 
-case class Position(_1: Int, _2: Int)
+case class Position(x: Int, y: Int)
 
 sealed class Direction
 case object North extends Direction
