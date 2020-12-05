c04b78349f5ef8d82d0380f4a143c25bf038bd81
mgryszko <marcin.gryszko@lastminute.com>
2020-12-05 15:06:40 +0100

    Move forward as long as the position is (0, 0) and the rover is facing north

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -1,8 +1,21 @@
-import cats.syntax.all._
 import org.scalatest.funsuite.AnyFunSuite
 
 class Test extends AnyFunSuite {
-  test("test is run") {
-    assert(true.show == false.show)
+  import MarsRover.moveForward
+
+  test("move forward") {
+    assert(moveForward(MarsRover((0, 0), North)) == MarsRover((0, 1), North))
   }
 }
+
+object MarsRover {
+  def moveForward(rover: MarsRover): MarsRover = MarsRover((0, 1), North)
+}
+
+case class MarsRover(position: (Int, Int), direction: Direction)
+
+sealed class Direction
+case object North extends Direction
+case object East extends Direction
+case object South extends Direction
+case object West extends Direction
