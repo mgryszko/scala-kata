bcf504dd1e8ed642e87a8dc30d0be7a645a4ce9a
mgryszko <marcin.gryszko@lastminute.com>
2020-12-05 15:15:20 +0100

    Move backward without wrapping

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -1,13 +1,20 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 class Test extends AnyFunSuite {
-  import MarsRover.moveForward
+  import MarsRover._
 
   test("move forward") {
     assert(moveForward(MarsRover((0, 0), North)) == MarsRover((0, 1), North))
     assert(moveForward(MarsRover((0, 0), East)) == MarsRover((1, 0), East))
     assert(moveForward(MarsRover((0, 0), South)) == MarsRover((0, -1), South))
     assert(moveForward(MarsRover((0, 0), West)) == MarsRover((-1, 0), West))
+  }
+
+  test("move backward") {
+    assert(moveBackward(MarsRover((0, 0), North)) == MarsRover((0, -1), North))
+    assert(moveBackward(MarsRover((0, 0), East)) == MarsRover((-1, 0), East))
+    assert(moveBackward(MarsRover((0, 0), South)) == MarsRover((0, 1), South))
+    assert(moveBackward(MarsRover((0, 0), West)) == MarsRover((1, 0), West))
   }
 }
 
@@ -17,6 +24,13 @@ object MarsRover {
     case East =>rover.copy(position = (rover.position._1 + 1, rover.position._2))
     case South =>rover.copy(position = (rover.position._1, rover.position._2 - 1))
     case West =>rover.copy(position = (rover.position._1 - 1, rover.position._2))
+  }
+
+  def moveBackward(rover: MarsRover): MarsRover = rover.direction match {
+    case North => rover.copy(position = (rover.position._1, rover.position._2 - 1))
+    case East =>rover.copy(position = (rover.position._1 - 1, rover.position._2))
+    case South =>rover.copy(position = (rover.position._1, rover.position._2 + 1))
+    case West =>rover.copy(position = (rover.position._1 + 1, rover.position._2))
   }
 }
 
