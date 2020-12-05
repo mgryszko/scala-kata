f4978c594a19cd371ef420794bfb3014691bad90
mgryszko <marcin.gryszko@lastminute.com>
2020-12-05 15:55:26 +0100

    Move as module public API

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -4,55 +4,27 @@ class Test extends AnyFunSuite {
   import MarsRover._
 
   test("move forward") {
-    assert(moveForward(MarsRover(Position(0, 0), North)) == MarsRover(Position(0, 1), North))
-    assert(moveForward(MarsRover(Position(0, 0), East)) == MarsRover(Position(1, 0), East))
-    assert(moveForward(MarsRover(Position(0, 0), South)) == MarsRover(Position(0, -1), South))
-    assert(moveForward(MarsRover(Position(0, 0), West)) == MarsRover(Position(-1, 0), West))
-  }
-
-  test("move backward") {
-    assert(moveBackward(MarsRover(Position(0, 0), North)) == MarsRover(Position(0, -1), North))
-    assert(moveBackward(MarsRover(Position(0, 0), East)) == MarsRover(Position(-1, 0), East))
-    assert(moveBackward(MarsRover(Position(0, 0), South)) == MarsRover(Position(0, 1), South))
-    assert(moveBackward(MarsRover(Position(0, 0), West)) == MarsRover(Position(1, 0), West))
-  }
-
-  test("turn right") {
-    assert(turnRight(MarsRover(Position(0, 0), North)) == MarsRover(Position(0, 0), East))
-    assert(turnRight(MarsRover(Position(0, 0), East)) == MarsRover(Position(0, 0), South))
-    assert(turnRight(MarsRover(Position(0, 0), South)) == MarsRover(Position(0, 0), West))
-    assert(turnRight(MarsRover(Position(0, 0), West)) == MarsRover(Position(0, 0), North))
-  }
-
-  test("turn left") {
-    assert(turnLeft(MarsRover(Position(0, 0), North)) == MarsRover(Position(0, 0), West))
-    assert(turnLeft(MarsRover(Position(0, 0), East)) == MarsRover(Position(0, 0), North))
-    assert(turnLeft(MarsRover(Position(0, 0), South)) == MarsRover(Position(0, 0), East))
-    assert(turnLeft(MarsRover(Position(0, 0), West)) == MarsRover(Position(0, 0), South))
-  }
-
-  test("single forward command") {
     assert(move(Forward, MarsRover(Position(0, 0), North)) == MarsRover(Position(0, 1), North))
     assert(move(Forward, MarsRover(Position(0, 0), East)) == MarsRover(Position(1, 0), East))
     assert(move(Forward, MarsRover(Position(0, 0), South)) == MarsRover(Position(0, -1), South))
     assert(move(Forward, MarsRover(Position(0, 0), West)) == MarsRover(Position(-1, 0), West))
   }
 
-  test("single backward command") {
+  test("move backward") {
     assert(move(Backward, MarsRover(Position(0, 0), North)) == MarsRover(Position(0, -1), North))
     assert(move(Backward, MarsRover(Position(0, 0), East)) == MarsRover(Position(-1, 0), East))
     assert(move(Backward, MarsRover(Position(0, 0), South)) == MarsRover(Position(0, 1), South))
     assert(move(Backward, MarsRover(Position(0, 0), West)) == MarsRover(Position(1, 0), West))
   }
 
-  test("single turn right command") {
+  test("turn right") {
     assert(move(Right, MarsRover(Position(0, 0), North)) == MarsRover(Position(0, 0), East))
     assert(move(Right, MarsRover(Position(0, 0), East)) == MarsRover(Position(0, 0), South))
     assert(move(Right, MarsRover(Position(0, 0), South)) == MarsRover(Position(0, 0), West))
     assert(move(Right, MarsRover(Position(0, 0), West)) == MarsRover(Position(0, 0), North))
   }
 
-  test("single turn left command") {
+  test("turn left") {
     assert(move(Left, MarsRover(Position(0, 0), North)) == MarsRover(Position(0, 0), West))
     assert(move(Left, MarsRover(Position(0, 0), East)) == MarsRover(Position(0, 0), North))
     assert(move(Left, MarsRover(Position(0, 0), South)) == MarsRover(Position(0, 0), East))
@@ -68,23 +40,23 @@ object MarsRover {
     case Right => turnRight(rover)
   }
 
-  def moveForward(rover: MarsRover): MarsRover = rover.direction match {
+  private def moveForward(rover: MarsRover): MarsRover = rover.direction match {
     case North => rover.copy(position = rover.position.up)
     case East => rover.copy(position = rover.position.right)
     case South => rover.copy(position = rover.position.down)
     case West => rover.copy(position = rover.position.left)
   }
 
-  def moveBackward(rover: MarsRover): MarsRover = rover.direction match {
+  private def moveBackward(rover: MarsRover): MarsRover = rover.direction match {
     case North => rover.copy(position = rover.position.down)
     case East => rover.copy(position = rover.position.left)
     case South => rover.copy(position = rover.position.up)
     case West => rover.copy(position = rover.position.right)
   }
 
-  def turnRight(rover: MarsRover): MarsRover = rover.copy(direction = rover.direction.right)
+  private def turnRight(rover: MarsRover): MarsRover = rover.copy(direction = rover.direction.right)
 
-  def turnLeft(rover: MarsRover): MarsRover = rover.copy(direction = rover.direction.left)
+  private def turnLeft(rover: MarsRover): MarsRover = rover.copy(direction = rover.direction.left)
 }
 
 sealed trait Command
