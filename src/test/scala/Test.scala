89a44e83c2eb7f42419043830a2b0103904b23bf
mgryszko <marcin.gryszko@lastminute.com>
2021-03-26 19:41:05 +0100

    alive cell, 0 alive neighbours

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -1,8 +1,21 @@
+import cats._
+import cats.data._
+import cats.implicits._
 import cats.syntax.all._
 import org.scalatest.funsuite.AnyFunSuite
 
+object GameOfLife {
+  sealed trait Cell
+  object Alive extends Cell
+  object Dead extends Cell
+
+  def step(cell: Cell, aliveNeighbours: Int): Cell = Dead
+}
+
 class Test extends AnyFunSuite {
-  test("test is run") {
-    assert(true.show == false.show)
+  import GameOfLife._
+
+  test("single cell with 0 alive neighbours") {
+    assert(step(Alive, 0) === Dead)
   }
 }
