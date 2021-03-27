522d0a908909d31b41c84f29c0c1892a86ac8766
mgryszko <marcin.gryszko@lastminute.com>
2021-03-27 11:26:34 +0100

    Etude: board representation as sequence of alive cells

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -9,10 +9,15 @@ object GameOfLife {
   object Alive extends State
   object Dead extends State
 
+  type Position = (Int, Int)
+
   def step(cell: State, aliveNeighbours: Int): State = cell match {
     case Alive => if (aliveNeighbours == 2 || aliveNeighbours == 3) Alive else Dead
     case Dead => if (aliveNeighbours == 3) Alive else Dead
   }
+
+  def cellAt(board: List[Position], pos: Position): State =
+    if (board.contains(pos)) Alive else Dead
 }
 
 class Test extends AnyFunSpec {
@@ -49,6 +54,30 @@ class Test extends AnyFunSpec {
 
     it("dead cell with 4 alive neighbours") {
       assert(step(Dead, 4) === Dead)
+    }
+  }
+
+  describe("board") {
+    it("dead or alive cells") {
+      val blinker = List((0, 0) ,(0, 1), (0, 2))
+
+      assert(cellAt(blinker, (0, 0)) === Alive)
+      assert(cellAt(blinker, (0, 1)) === Alive)
+      assert(cellAt(blinker, (0, 2)) === Alive)
+
+      assert(cellAt(blinker, (-1, 0)) === Dead)
+      assert(cellAt(blinker, (-1, -1)) === Dead)
+      assert(cellAt(blinker, (0, -1)) === Dead)
+      assert(cellAt(blinker, (1, -1)) === Dead)
+      assert(cellAt(blinker, (1, 0)) === Dead)
+      assert(cellAt(blinker, (1, 1)) === Dead)
+
+      assert(cellAt(blinker, (-1, 1)) === Dead)
+      assert(cellAt(blinker, (-1, 2)) === Dead)
+      assert(cellAt(blinker, (-1, 3)) === Dead)
+      assert(cellAt(blinker, (0, 3)) === Dead)
+      assert(cellAt(blinker, (1, 3)) === Dead)
+      assert(cellAt(blinker, (1, 2)) === Dead)
     }
   }
 }
