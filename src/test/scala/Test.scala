6bb6fd75c05bb1cf15e943488e3b1072da46d908
mgryszko <marcin.gryszko@lastminute.com>
2021-03-29 22:57:28 +0200

    Get rid of cell concept; calculate position liveness during next generation computation

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -10,7 +10,7 @@ object GameOfLife {
     override def toString: String = "A"
   }
   object Dead extends State {
-    override def toString: String ="D"
+    override def toString: String = "D"
   }
 
   case class Position(x: Int, y: Int) {
@@ -19,56 +19,37 @@ object GameOfLife {
     lazy val west: Position = Position(x + 1, y)
     lazy val southWest: Position = Position(x + 1, y - 1)
     lazy val south: Position = Position(x, y - 1)
-    lazy val southEast: Position = Position(x - 1 , y - 1)
+    lazy val southEast: Position = Position(x - 1, y - 1)
     lazy val east: Position = Position(x - 1, y)
     lazy val northEast: Position = Position(x - 1, y + 1)
 
-    def alive: Cell = Cell(this, Alive)
-    def dead: Cell = Cell(this, Dead)
+    def neighbours: List[Position] =
+      List(north, northWest, west, southWest, south, southEast, east, northEast)
 
     override def toString: String = s"($x,$y)"
   }
 
-  case class Cell(pos: Position, state: State) {
-    override def toString: String = s"$pos-$state"
-  }
-
   type Population = Set[Position]
 
-  def nextGen(population: Population): Population =
-    meAndNeighbours(population).flatMap { cell =>
-      val alive = aliveNeighbours(cell.pos, population)
-      cell match {
-        case Cell(pos, Alive) => if (alive == 2 || alive == 3) Some(pos) else None
-        case Cell(pos, Dead) => if (alive == 3) Some(pos) else None
+  def nextGen(population: Population): Population = {
+    expansionArea(population).flatMap { pos =>
+      val state = if (population.contains(pos)) Alive else Dead
+      val alive = aliveNeighbours(pos, population)
+      state match {
+        case Alive => if (alive == 2 || alive == 3) Some(pos) else None
+        case Dead => if (alive == 3) Some(pos) else None
       }
     }
-
-  private def aliveNeighbours(pos: Position, population: Population): Int =
-    (neighbours(pos).toSet & population).size
-
-  def meAndNeighbours(population: Population): Set[Cell] = {
-    val allNeighbours = for {
-      alivePosition <- population
-      neighbour <- neighbours(alivePosition)
-    } yield toCell(neighbour, population)
-    (allNeighbours ++ population.map(_.alive)).toSet
   }
 
-  private def neighbours(pos: Position): List[Position] =
-    List(
-      pos.north,
-      pos.northWest,
-      pos.west,
-      pos.southWest,
-      pos.south,
-      pos.southEast,
-      pos.east,
-      pos.northEast,
-    )
-    
-  private def toCell(pos: Position, population: Population): Cell =
-    if (population.contains(pos)) pos.alive else pos.dead
+  private def expansionArea(population: Population): Population =
+    for {
+      pos <- population
+      neighbour <- pos.neighbours
+    } yield neighbour
+
+  private def aliveNeighbours(pos: Position, population: Population): Int =
+    (pos.neighbours.toSet & population).size
 }
 
 class Test extends AnyFunSpec {
